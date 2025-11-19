fd722ee21a95df8c8f536163ca6a546ecb867d1f
Razil kp <rasrasil69@gmail.com>
2025-11-19 17:49:29 +0530

    database checking

--- config/db.js
+++ config/db.js
@@ -3,7 +3,13 @@ const mongoose = require('mongoose');
 const connectDB = async () => {
   const dbUrl = process.env.MongoDB_URL;
   try {
-    await mongoose.connect(dbUrl)
+    mongoose.connection.on('connected', () => {
+      console.log('Mongoose connected to DB');
+    });
+    await mongoose.connect(dbUrl,{
+      serverSelectionTimeoutMS: 5000,
+      socketTimeoutMS: 45000,
+    })
     console.log('MongoDB connected successfully');
   } catch (error) {
     console.log(error);
